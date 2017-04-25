c2c6977755da787ec5842a8390b70f7a6e5ea827
ldc84 <ldc84@naver.com>
2017-04-25 18:03:06 +0900

    index video add

--- js/main.js
+++ js/main.js
@@ -103,7 +103,7 @@ function scaleVideoContainer() {
 
     var height = $(window).height() + 5;
     var unitHeight = parseInt(height) + 'px';
-    $('.homepage-hero-module').css('height',unitHeight);
+    // $('.homepage-hero-module').css('height',unitHeight);
 
 }
 
